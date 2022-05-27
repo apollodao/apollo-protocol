e9d447238cfc13ecd0f30d4a25d3d5bb1a2d049a
rishablal <lal.rishab@gmail.com>
2022-05-26 20:04:41 -0400

    chore - removes graph and router

--- src/graph/mod.rs
+++
@@ -1,6 +0,0 @@
-pub mod algo;
-pub mod entities;
-pub mod state;
-
-#[cfg(test)]
-mod tests;
--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,10 @@
 pub mod adaptor;
 pub mod collector;
 pub mod factory;
-pub mod graph;
 pub mod legacy_strategy;
 pub mod legacy_vault;
 pub mod oracle;
 pub mod querier;
-pub mod router;
 pub mod strategy;
 pub mod strategy_token;
 pub mod utils;
